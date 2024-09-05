7ec157158c9d88780ad13b15db14ee63a1cf5878
Álvaro García Pizarro <alvarogp99@gmail.com>
2024-09-05 14:06:25 +0200

    chore: Update workflow template with lazy loading for images

---
+++ test/test1.js
@@ -0,0 +1,8 @@
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+
+describe('Sample Test', function() {
+  it('should return true', function() {
+    expect(true).to.be.true;
+  });
+});
