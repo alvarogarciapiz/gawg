da9c37249fc4935348e2fced7db275d05bd5bdba
Álvaro García Pizarro <alvarogp99@gmail.com>
2024-09-14 19:21:39 +0200

    Refactor YouTube component to use lite-youtube-embed library and lazy loading

--- src/components/youtube.jsx
+++ src/components/youtube.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
+import 'lite-youtube-embed';
 import '../styles/youtube.css';
+import '../styles/lite-yt-embed.css';
+import YouTubeImg from '../assets/youtube.webp';
 
 const YouTube = () => {
   return (
     <div className="youtube_container">
       <h2 className="youtube_title">See Our Platform in Action</h2>
       <p className="youtube_subtitle">
-      Let us show you how simple it is to get started with GAWG in this quick demo.
+        Let us show you how simple it is to get started with GAWG in this quick demo.
       </p>
       <div className="youtube_video">
-        <iframe
-          width="1080"
-          height="720"
-          src="https://www.youtube.com/embed/URmeTqglS58"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-          loading="lazy"
-        ></iframe>
+        <lite-youtube 
+          videoid="NrQkdDVupQE" 
+          style={{ 
+            width: '100%', 
+            height: 'auto', 
+            aspectRatio: '16/9', 
+            backgroundImage: `url(${YouTubeImg})`,
+          }}
+        ></lite-youtube>
       </div>
     </div>
   );
