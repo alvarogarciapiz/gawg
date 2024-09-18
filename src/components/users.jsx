c7dad85b4ffb4bf0c2427ecf5adba975493478ea
Álvaro García Pizarro <81751965+alvarogarciapiz@users.noreply.github.com>
2024-09-18 12:13:27 +0200

    Update users.jsx

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -5,7 +5,7 @@ import usersImage from '../assets/users.webp';
 const Users = () => {
     return (
         <div className="users_container">
-            <h2 className="users_title">Fueling 14 Projects with 15M+ Lines of Code</h2>
+            <h2 className="users_title">Fueling your project with 15M+ Lines of Code</h2>
             <p className="users_subtitle">
                 Join the growing community leveraging a versatile codebase of 15 million lines, compatible with Python, Java, Node.js, and more.
             </p>
