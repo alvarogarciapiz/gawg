6a07814af51d7f02f2d9d7b6b4d2f05bb4b896cc
Álvaro García Pizarro <alvarogp99@gmail.com>
2024-09-18 17:11:41 +0200

    Refactor hero component and styles
    - Update hero component to include a new image and adjust layout
    - Modify hero button margins for better spacing on different devices
    - Adjust hero image width for responsiveness on different screen sizes

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,14 +4,17 @@ import HERO from '../assets/HERO.webp';
 
 const Hero = () => {
   return (
+    <>
     <div className="hero_container">
       <h1 className="hero_title">Simplify CI/CD with automated GitHub actions Workflows</h1>
       <p className="hero_paragraph">
         Automate your GitHub Actions with GAWG, the tool that converts your project needs into efficient GitHub Actions workflows.
       </p>
       <a href="#get-started" className="hero_button get_started">Get started</a>
-      <img src={HERO} alt="CI/CD Illustration" className="hero_image" />
+      
     </div>
+    <img src={HERO} alt="CI/CD Illustration" className="hero_image" />
+    </>
   );
 };
 
